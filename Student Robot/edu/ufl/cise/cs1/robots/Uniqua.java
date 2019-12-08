eb9aea4774706d20db7e31cf8b16a47c35a7ee59
wuseokjung <wuseok.jung@gmail.com>
2019-12-08 18:35:46 -0500

    Pasted from my own IntelliJ

--- Student Robot/edu/ufl/cise/cs1/robots/Uniqua.java
+++ Student Robot/edu/ufl/cise/cs1/robots/Uniqua.java
@@ -1,8 +1,33 @@
 package edu.ufl.cise.cs1.robots;
 
-import robocode.*;
+import robocode.ScannedRobotEvent;
+import robocode.TeamRobot;
 
-public class Uniqua extends TeamRobot
-{
+import java.awt.*;
 
+public class Uniqua extends TeamRobot {
+    public void setColors() {
+        setGunColor(Color.CYAN);
+        setBodyColor(Color.CYAN);
+        setRadarColor(Color.CYAN);          //Team Color
+        setBulletColor(Color.CYAN);         //Team Color
+        setScanColor(Color.CYAN);
+    }
+
+    public void run() {
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        if(isTeammate(e.getName())) {
+
+        }
+        else
+            fire(1);
+    }
 }
