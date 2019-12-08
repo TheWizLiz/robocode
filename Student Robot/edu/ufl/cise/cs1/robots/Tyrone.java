3bc25a0562abeeb5248d0016cfcaef069a69dfe5
jefferyroberts <jefferyroberts@ufl.edu>
2019-12-08 18:17:20 -0500

    i copy and pasted

--- Student Robot/edu/ufl/cise/cs1/robots/Tyrone.java
+++ Student Robot/edu/ufl/cise/cs1/robots/Tyrone.java
@@ -1,8 +1,38 @@
 package edu.ufl.cise.cs1.robots;
 
 import robocode.*;
+import robocode.util.Utils;
 
-public class Trone extends TeamRobot
+<<<<<<< HEAD
+import java.awt.*;
+
+=======
+>>>>>>> fa0e7f89e7d384712e52be76558a735a32d32b4f
+public class Tyrone extends TeamRobot
 {
+    public void run() {
+        setBodyColor(Color.cyan);
+        setGunColor(Color.cyan);
+        setRadarColor(Color.cyan);
+        setBulletColor(Color.cyan);
+        setScanColor(Color.cyan);
 
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+
+        setTurnRadarRight(Double.POSITIVE_INFINITY);
+
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(3);
+        setTurnRadarRight(2.0 * Utils.normalRelativeAngleDegrees(getHeading() + e.getBearing() - getRadarHeading()));
+    }
 }
+
+
--- Student Robot/edu/ufl/cise/cs1/robots/Uniqua.java
+++ Student Robot/edu/ufl/cise/cs1/robots/Uniqua.java
@@ -1,6 +1,6 @@
 package edu.ufl.cise.cs1.robots;
 
-import robocode.*
+import robocode.*;
 
 public class Uniqua extends TeamRobot
 {
