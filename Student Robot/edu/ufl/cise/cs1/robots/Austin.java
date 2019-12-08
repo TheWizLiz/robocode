2bf68d781cf517b05fc732734949eb6b477c1906
TheWizLiz <57337009+TheWizLiz@users.noreply.github.com>
2019-12-08 18:49:07 -0500

    Changed body color

--- Student Robot/edu/ufl/cise/cs1/robots/Austin.java
+++ Student Robot/edu/ufl/cise/cs1/robots/Austin.java
@@ -1,8 +1,42 @@
 package edu.ufl.cise.cs1.robots;
 
 import robocode.*;
+import robocode.util.Utils;
+
+import java.awt.*;
 
 public class Austin extends TeamRobot
 {
+
+    public void run() {
+
+        setBodyColor(Color.cyan);
+        setGunColor(Color.cyan);
+        setRadarColor(Color.cyan);
+        setBulletColor(Color.cyan);
+        setScanColor(Color.cyan);
+
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+
+        setTurnRadarRight(Double.POSITIVE_INFINITY);
+
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+    public void onScannedRobot(ScannedRobotEvent e) {
+        if (isTeammate(e.getName())) {
+            scan();
+        } else {
+            fire(2);
+        }
+        setMaxVelocity(8);
+        setTurnRadarRight(2.0 * Utils.normalRelativeAngleDegrees(getHeading() + e.getBearing() - getRadarHeading()));
+
+    }
 
 }
