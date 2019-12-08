a62094d89aa79bbd88db4490e8b4f2eb0ed4bb6b
TheWizLiz <57337009+TheWizLiz@users.noreply.github.com>
2019-12-08 18:33:03 -0500

    Color Setting and Gitignore changes

--- Student Robot/edu/ufl/cise/cs1/robots/Austin.java
+++ Student Robot/edu/ufl/cise/cs1/robots/Austin.java
@@ -2,10 +2,15 @@ package edu.ufl.cise.cs1.robots;
 
 import robocode.*;
 
+import java.awt.*;
+
 public class Austin extends TeamRobot
 {
 
     public void run() {
+
+        setColors(Color.cyan, Color.cyan, Color.cyan, Color.cyan, Color.cyan);
+
         while (true) {
             ahead(100);
             turnGunRight(360);
@@ -15,7 +20,11 @@ public class Austin extends TeamRobot
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+        if (!isTeammate(e.getName())) {
+            fire(1);
+        } else {
+            scan();
+        }
     }
 
 }
