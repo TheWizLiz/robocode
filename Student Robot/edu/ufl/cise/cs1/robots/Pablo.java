bca0b0f37462d433555e8436bb9478a9b9c66282
jakew54 <57453113+jakew54@users.noreply.github.com>
2019-12-09 13:05:59 -0500

    pabloid

--- Student Robot/edu/ufl/cise/cs1/robots/Pablo.java
+++ Student Robot/edu/ufl/cise/cs1/robots/Pablo.java
@@ -1,33 +1,36 @@
 package edu.ufl.cise.cs1.robots;
 
 import robocode.*;
+import sampleteam.Point;
+import sampleteam.RobotColors;
+import sun.plugin2.message.Message;
+
+import static robocode.util.Utils.*;
 import java.awt.*;
 
-public class Pablo extends TeamRobot implements Droid {
+public class Pablo extends TeamRobot implements Droid{
     public void run() {
         setBodyColor(Color.PINK);
         setGunColor(Color.PINK);
-        setRadarColor(Color.cyan);
-        setBulletColor(Color.cyan);
-        setScanColor(Color.PINK);
-        while (true) {
 
+    }
+
+    public void onMessageReceived(MessageEvent e) {
+        if (e.getMessage() instanceof Point) {
+            Point p = (Point) e.getMessage();
+            double dx = p.getX() - this.getX();
+            double dy = p.getY() - this.getY();
+            double theta = Math.toDegrees(Math.atan2(dx, dy));
+
+            turnGunRight(normalRelativeAngleDegrees(theta - getGunHeading()));
+            fire(3);
+
+        } else if (e.getMessage() instanceof RobotColors) {
+            RobotColors c = (RobotColors) e.getMessage();
+
+            setRadarColor(c.radarColor);
+            setScanColor(c.scanColor);
+            setBulletColor(c.bulletColor);
         }
-    }
-
-    public void onScannedRobot(ScannedRobotEvent e) {
-       if (isTeammate(e.getName())) {
-           scan();
-       } else {
-                
-       }
-    }
-
-    public void onHitByBullet(HitByBulletEvent e) {
-        back(10);
-    }
-
-    public void onHitWall(HitWallEvent e) {
-        back(20);
     }
 }
