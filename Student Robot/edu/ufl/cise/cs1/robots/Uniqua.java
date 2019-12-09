3ca31534559dd74d387b96c945c934a0b507883b
wuseokjung <wuseok.jung@gmail.com>
2019-12-09 14:15:12 -0500

    Taking Pablo's role

--- Student Robot/edu/ufl/cise/cs1/robots/Uniqua.java
+++ Student Robot/edu/ufl/cise/cs1/robots/Uniqua.java
@@ -3,78 +3,55 @@ package edu.ufl.cise.cs1.robots;
 import robocode.*;
 import sampleteam.Point;
 import sampleteam.RobotColors;
+import sun.plugin2.message.Message;
 
+import static robocode.util.Utils.*;
 import java.awt.*;
 
-import static robocode.util.Utils.normalRelativeAngleDegrees;
-
-public class Uniqua extends TeamRobot implements Droid{
-    public void setColors() {
-        setGunColor(Color.PINK);
+public class Uniqua extends TeamRobot implements Droid {
+    int turn;
+    public void run() {
         setBodyColor(Color.PINK);
-        setRadarColor(Color.CYAN);          //Team Color
-        setBulletColor(Color.CYAN);         //Team Color
-        setScanColor(Color.PINK);
+        setGunColor(Color.PINK);
     }
 
-    public void run() {
-        while (true) {
-//            turnRight(90);
-//            ahead(100);
-//            turnGunRight(360);
-//            turnRight(90);
-//            ahead(100);
-//            turnGunRight(360);
+    public void onHitRobot(HitRobotEvent e) {
+        if (isTeammate(e.getName())) {
+            back(15);
+        } else {
+            turnRight(e.getBearing());
+            fire(3);
+            ahead(5);
         }
     }
 
     public void onMessageReceived(MessageEvent e) {
-        // Fire at a point
         if (e.getMessage() instanceof Point) {
             Point p = (Point) e.getMessage();
-            // Calculate x and y to target
             double dx = p.getX() - this.getX();
             double dy = p.getY() - this.getY();
-            // Calculate angle to target
             double theta = Math.toDegrees(Math.atan2(dx, dy));
 
-            // Turn gun to target
+            if (theta <= 180) {
+                turnRight(normalRelativeAngleDegrees(theta - getGunHeading()));
+                ahead(Math.sqrt(Math.pow((p.getX() - this.getX()), 2) - Math.pow((p.getY() - this.getY()), 2)) + 5);
+                if ((Math.sqrt(Math.pow((p.getX() - this.getX()), 2) - Math.pow((p.getY() - this.getY()), 2)) + 5) <= 10) {
+                    fire(1);
+                }
+            } else {
+                turnLeft(normalRelativeAngleDegrees(theta - getGunHeading()));
+                ahead(Math.sqrt(Math.pow((p.getX() - this.getX()), 2) - Math.pow((p.getY() - this.getY()), 2)) + 5);
+                if ((Math.sqrt(Math.pow((p.getX() - this.getX()), 2) - Math.pow((p.getY() - this.getY()), 2)) + 5) <= 10) {
+                    fire(1);
+                }
+            }
 
-            turnRight(normalRelativeAngleDegrees(theta - getGunHeading()));
-            turnGunRight(normalRelativeAngleDegrees(theta - getGunHeading()));
-            ahead(Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)));
-            fire(1);
-        } // Set our colors
-        else if (e.getMessage() instanceof RobotColors) {
+        } else if (e.getMessage() instanceof RobotColors) {
             RobotColors c = (RobotColors) e.getMessage();
 
-            setBodyColor(c.bodyColor);
-            setGunColor(c.gunColor);
             setRadarColor(c.radarColor);
             setScanColor(c.scanColor);
             setBulletColor(c.bulletColor);
         }
-    }
-
-//    public void onScannedRobot(ScannedRobotEvent e) {
-//        if(isTeammate(e.getName())) {
-//            scan();
-//        }
-//        else {
-//            setAhead(100);
-//            fire(3);
-//        }
-//    }
-
-    public void onHitRobot(HitRobotEvent event) {
-        back(20);
-    }
-
-    public void onHitByBullet(HitByBulletEvent e) {
-        back(10);
-    }
-
-    public void onHitWall(HitWallEvent e)  {
-        back(20);
     }
 }
