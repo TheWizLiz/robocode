3f9cd686198a217de4858c962d4acb68eb8b6b9d
jefferyroberts <jefferyroberts@ufl.edu>
2019-12-09 16:06:10 -0500

    s

--- Student Robot/edu/ufl/cise/cs1/robots/Tyrone.java
+++ Student Robot/edu/ufl/cise/cs1/robots/Tyrone.java
@@ -11,9 +11,133 @@ import java.awt.*;
 
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
-public class Tyrone extends TeamRobot implements Droid {
+public class Tyrone extends TeamRobot {
+    int others; // Number of other robots in the game
+    static int corner = 0; // Which corner we are currently using
+    // static so that it keeps it between rounds.
+    boolean stopWhenSeeRobot = false; // See goCorner()
+
+    /**
+     * run:  Corners' main run function.
+     */
+    public void run() {
+        // Set colors
+        setBodyColor(Color.red);
+        setGunColor(Color.black);
+        setRadarColor(Color.yellow);
+        setBulletColor(Color.green);
+        setScanColor(Color.green);
+
+        // Save # of other bots
+        others = getOthers();
+
+        // Move to a corner
+        goCorner();
+
+        // Initialize gun turn speed to 3
+        int gunIncrement = 3;
+
+        // Spin gun back and forth
+        while (true) {
+            for (int i = 0; i < 30; i++) {
+                turnGunLeft(gunIncrement);
+            }
+            gunIncrement *= -1;
+        }
+    }
+
+    /**
+     * goCorner:  A very inefficient way to get to a corner.  Can you do better?
+     */
+    public void goCorner() {
+        // We don't want to stop when we're just turning...
+        stopWhenSeeRobot = false;
+        // turn to face the wall to the "right" of our desired corner.
+        turnRight(normalRelativeAngleDegrees(corner - getHeading()));
+        // Ok, now we don't want to crash into any robot in our way...
+        stopWhenSeeRobot = true;
+        // Move to that wall
+        ahead(5000);
+        // Turn to face the corner
+        turnLeft(90);
+        // Move to the corner
+        ahead(5000);
+        // Turn gun to starting point
+        turnGunLeft(90);
+    }
+
+    /**
+     * onScannedRobot:  Stop and fire!
+     */
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Should we stop, or just fire?
+        if (stopWhenSeeRobot) {
+            // Stop everything!  You can safely call stop multiple times.
+            stop();
+            // Call our custom firing method
+            smartFire(e.getDistance());
+            // Look for another robot.
+            // NOTE:  If you call scan() inside onScannedRobot, and it sees a robot,
+            // the game will interrupt the event handler and start it over
+            scan();
+            // We won't get here if we saw another robot.
+            // Okay, we didn't see another robot... start moving or turning again.
+            resume();
+        } else {
+            smartFire(e.getDistance());
+        }
+    }
+
+    /**
+     * smartFire:  Custom fire method that determines firepower based on distance.
+     *
+     * @param robotDistance the distance to the robot to fire at
+     */
+    public void smartFire(double robotDistance) {
+        if (robotDistance > 200 || getEnergy() < 15) {
+            fire(1);
+        } else if (robotDistance > 50) {
+            fire(2);
+        } else {
+            fire(3);
+        }
+    }
+
+    /**
+     * onDeath:  We died.  Decide whether to try a different corner next game.
+     */
+    public void onDeath(DeathEvent e) {
+        // Well, others should never be 0, but better safe than sorry.
+        if (others == 0) {
+            return;
+        }
+
+        // If 75% of the robots are still alive when we die, we'll switch corners.
+        if ((others - getOthers()) / (double) others < .75) {
+            corner += 90;
+            if (corner == 270) {
+                corner = -90;
+            }
+            out.println("I died and did poorly... switching corner to " + corner);
+        } else {
+            out.println("I died but did well.  I will still use corner " + corner);
+        }
+    }
+}
+
+/* }
+
+        public void goToCorner() {
+
+            turnRight(normalRelativeAngleDegrees(0-getHeading()));
+
+            ahead(2000);
+            turnLeft(90);
+            ahead(2000);
+            turnGunLeft(90);
+        }
         public void run() {
-            /* setBodyColor(Color.cyan);
+            setBodyColor(Color.cyan);
             setGunColor(Color.cyan);
             setRadarColor(Color.cyan);
             setBulletColor(Color.cyan);
@@ -30,17 +154,20 @@ public class Tyrone extends TeamRobot implements Droid {
                 turnGunRight(360);
                 back(100);
                 turnGunRight(360);
-            } */
+            }
         }
 
     public void onHitRobot(HitRobotEvent e) {
         if (isTeammate(e.getName())) {
             back(50);
         }
-        setMaxVelocity(100);
-        ahead(1000);
-        fire(3);
+        if (e.getBearing() > -10 && e.getBearing() < 10) {
+            fire(3);
+            setMaxVelocity(100);
+            ahead(1000);
 
+
+        }
     }
 
     public void onHitWall(HitWallEvent e) {
@@ -50,7 +177,7 @@ public class Tyrone extends TeamRobot implements Droid {
             ahead(300);
     }
 
-    public void onMessageReceived(MessageEvent e) {
+    /* public void onMessageReceived(MessageEvent e) {
         // Fire at a point
         if (e.getMessage() instanceof Point) {
             Point p = (Point) e.getMessage();
@@ -90,31 +217,25 @@ public class Tyrone extends TeamRobot implements Droid {
         }
     }
 
-        public void onScannedRobot(ScannedRobotEvent e) {
-            /* if (isTeammate(e.getName())) {
-                scan();
-
-            }
-
-            if (getDistanceRemaining() < 200) {
-                fire(3);
-            }
-            fire(1);
-
-            if (getEnergy() > 30) {
-                fire(3);
-            } else {
-                fire(1);
-            }
-
-            setTurnRadarRight(2.0 * Utils.normalRelativeAngleDegrees(getHeading() + e.getBearing() - getRadarHeading()));
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Should we stop, or just fire?
+        if (stopWhenSeeRobot) {
+            // Stop everything!  You can safely call stop multiple times.
+            stop();
+            // Call our custom firing method
+            smartFire(e.getDistance());
+            // Look for another robot.
+            // NOTE:  If you call scan() inside onScannedRobot, and it sees a robot,
+            // the game will interrupt the event handler and start it over
+            scan();
+            // We won't get here if we saw another robot.
+            // Okay, we didn't see another robot... start moving or turning again.
+            resume();
+        } else {
+            smartFire(e.getDistance());
         }
 
-        public void whenHitByBullet(HitByBulletEvent e) {
-            turnLeft(45);
-            back(50); */
 
-        }
     }
 
-
+*/
