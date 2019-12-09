21620c09690301f7d7722580b4122d243a650521
wuseokjung <wuseok.jung@gmail.com>
2019-12-09 15:41:43 -0500

    Commit to UF

--- Student Robot/edu/ufl/cise/cs1/robots/Uniqua.java
+++ Student Robot/edu/ufl/cise/cs1/robots/Uniqua.java
@@ -1,39 +1,59 @@
 package edu.ufl.cise.cs1.robots;
 
-import robocode.ScannedRobotEvent;
-import robocode.TeamRobot;
 import robocode.*;
-import sampleteam.Point;
-import sampleteam.RobotColors;
 
 import java.awt.*;
 
-import static robocode.util.Utils.normalRelativeAngleDegrees;
-
-public class Uniqua extends TeamRobot {
+public class Uniqua extends TeamRobot{
     public void setColors() {
-        setGunColor(Color.CYAN);
-        setBodyColor(Color.CYAN);
-        setRadarColor(Color.RED);          //Team Color
-        setGunColor(Color.BLACK);
-        setBodyColor(Color.GREEN);
-        setRadarColor(Color.CYAN);          //Team Color
-        setBulletColor(Color.CYAN);         //Team Color
-        setScanColor(Color.CYAN);
-        setScanColor(Color.BLUE);
+        setGunColor(Color.pink);
+        setBodyColor(Color.pink);
+        setRadarColor(Color.cyan);          //Team Color
+        setBulletColor(Color.cyan);         //Team Color
+        setScanColor(Color.pink);
     }
 
     public void run() {
         while (true) {
+            turnRight(90);
+            turnGunRight(360);
+            ahead(100);
+            turnRight(90);
             ahead(100);
             turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-            turnRight(90);
+
         }
     }
-    public void onHItByBullet(HitByBulletEvent e) {
-        back(10);
+
+    public void onBulletMissed(ScannedRobotEvent e) {
+        if(e.getDistance() < 40) {
+            turnRight(e.getBearing());
+            ahead(20);
+            fire(2);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        if(isTeammate(e.getName())) {
+            scan();
+        }
+        else {
+            setAhead(100);
+            fire(1);
+        }
+    }
+
+    public void onHitRobot(HitRobotEvent event) {
+        if(isTeammate(event.getName())) {
+            back(20);
+        }
+        else {
+            fireBullet(3);
+        }
+    }
+
+    public void onHitByBullet(HitByBulletEvent e) {
+        back(25);
     }
 
     public void onHitWall(HitWallEvent e)  {
